6871be82a49e810ba1a055dd9800c646178a2cfa
Abdallah Yahya <yabood358@gmail.com>
2024-04-11 17:05:04 +0200

    login code

--- src/main/java/org/xclone/HelloWorld.java
+++ src/main/java/org/xclone/HelloWorld.java
@@ -9,6 +9,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 import static io.javalin.rendering.template.TemplateUtil.model;
 
@@ -19,8 +20,7 @@ public class HelloWorld {
 
         Javalin app = Javalin.create(config -> {
                     config.fileRenderer(new JavalinPebble());
-                })
-                .get("/", ctx -> ctx.render("templates/templateFile.peb"))
+                  })
                 .get("/tweets", ctx -> {
 
                     String sql = "SELECT * FROM \"xcloneSchema\".\"tweet\"";
@@ -45,6 +45,51 @@ public class HelloWorld {
                     } catch (SQLException e) {
                         e.printStackTrace();
                     }
+                })
+                .get("/", ctx -> ctx.render("templates/login.peb"))
+                .post("/login",ctx->{
+                    // get username and password from form
+                    String email = ctx.formParam("email");
+                    String password = ctx.formParam("password");
+                    String user_name=null;
+                    String sql="SELECT * From \"xcloneSchema\".\"user\" where email = ?";
+                    boolean do_login=false;
+
+                    try (Connection conn = connection.getConnection(); // Assume this method returns a valid connection
+                         PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+                        pstmt.setString(1, email);
+
+                        try (ResultSet rs = pstmt.executeQuery()) {
+                            while (rs.next()) {
+                                // Assuming you're fetching a string from the first column of the result set
+                                String res_email = rs.getString("email");
+                                user_name=rs.getString("username");
+                                String res_password=rs.getString("password");
+
+                                if(Objects.equals(password, res_password)){
+                                    do_login=true;
+                                }
+                            }
+
+
+                        }
+                    } catch (SQLException e) {
+                        System.err.println("SQL Exception: " + e.getMessage());
+                    }
+                    if(do_login){
+                  //      System.out.println("inside");
+                        ctx.sessionAttribute("user", email);
+                        ctx.redirect("/homepage");
+                    }
+
+
+                })
+                .get("/homepage",ctx->{
+
+                   String email= ctx.sessionAttribute("user");
+                    ctx.render("templates/homepage.peb",model("email",email));
+
                 })
                 .start(8000);
     }
