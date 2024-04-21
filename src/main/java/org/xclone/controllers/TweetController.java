776cfb47885d39ad7619b27f129f1def2f5fa6e9
Abdallah Yahya <yabood358@gmail.com>
2024-04-21 16:48:09 +0300

    refactoring quries part 3

--- src/main/java/org/xclone/controllers/TweetController.java
+++ src/main/java/org/xclone/controllers/TweetController.java
@@ -12,41 +12,26 @@ import java.util.List;
 
 import static io.javalin.rendering.template.TemplateUtil.model;
 import org.jdbi.v3.core.Jdbi;
+import org.xclone.queries.TweetQuries;
 
 
 public class TweetController {
     private Jdbi jdbi;
+    private TweetQuries tweetQuries = new TweetQuries();
+
 
     public TweetController(Jdbi jdbi) {
         this.jdbi = jdbi;
     }
 
     public void renderHomepage(Context ctx) {
-        List<Tweet> tweets = jdbi.withHandle(handle ->
-                handle.createQuery("SELECT t.tweet_id, u.username, t.content, t.timestamp, t.location, t.media, t.in_reply_to_tweet_id, COUNT(l.like_id) as like_count " +
-                                "FROM \"xcloneSchema\".\"tweet\" t " +
-                                "JOIN \"xcloneSchema\".\"user\" u ON t.user_id = u.user_id " +
-                                "LEFT JOIN \"xcloneSchema\".\"like\" l ON t.tweet_id = l.tweet_id " +
-                                "GROUP BY t.tweet_id, u.username " +
-                                "ORDER BY t.timestamp DESC")
-                        .map((rs, mapCtx) -> new Tweet(
-                                rs.getString("tweet_id"),
-                                rs.getString("username"),
-                                rs.getString("content"),
-                                rs.getTimestamp("timestamp"),
-                                rs.getString("location"),
-                                rs.getString("media"),
-                                rs.getString("in_reply_to_tweet_id"),
-                                rs.getInt("like_count")
-                        ))
-                        .list()
-        );
+        List<Tweet> tweets = tweetQuries.tweetlist(jdbi);
         ctx.render("templates/homepage.peb", model("tweets", tweets));
     }
 
-
-
     public void handlePostCreate(Context ctx){
+
+
         String email = ctx.sessionAttribute("email");
         if (email == null) {
             ctx.render("templates/homepage.peb", model("errorMessage", "You must be logged in to post tweets."));
@@ -64,24 +49,14 @@ public class TweetController {
                 .orElse(0);
 
         jdbi.inTransaction(handle -> {
-            Integer userId = handle.createQuery("SELECT user_id FROM \"xcloneSchema\".\"user\" WHERE email = :email")
-                    .bind("email", email)
-                    .mapTo(Integer.class)
-                    .findOne()
-                    .orElse(null);
+            Integer userId = tweetQuries.getUserID(handle, email);
 
             if (userId == null) {
                 ctx.render("templates/homepage.peb", model("errorMessage", "User not found."));
-                return null; // Stop the transaction
+                return null;
             }
 
-            handle.createUpdate("INSERT INTO \"xcloneSchema\".\"tweet\" (user_id, content, timestamp, location, media, in_reply_to_tweet_id) VALUES (:userId, :content, NOW(), :location, :media, :replyToTweetId)")
-                    .bind("userId", userId)
-                    .bind("content", content)
-                    .bind("location", location)
-                    .bind("media", media)
-                    .bind("replyToTweetId", replyToTweetId==0 ? null : replyToTweetId)
-                    .execute();
+         tweetQuries.getNewTweet(handle, userId, content, location, media, replyToTweetId);
 
             ctx.redirect("/app/homepage");
             return null; // Complete the transaction
@@ -98,33 +73,19 @@ public class TweetController {
 
         int tweetId = Integer.parseInt(ctx.formParam("tweetId"));
         jdbi.inTransaction(handle -> {
-            Integer userId = handle.createQuery("SELECT user_id FROM \"xcloneSchema\".\"user\" WHERE email = :email")
-                    .bind("email", email)
-                    .mapTo(Integer.class)
-                    .findOne()
-                    .orElse(null);
+            Integer userId = tweetQuries.getUserID(handle, email);
 
             if (userId == null) {
                 ctx.render("templates/homepage.peb", model("errorMessage", "User not found."));
                 return null; // Stop the transaction
             }
 
-            boolean exists = handle.createQuery("SELECT COUNT(*) FROM \"xcloneSchema\".\"like\" WHERE tweet_id = :tweetId AND user_id = :userId")
-                    .bind("tweetId", tweetId)
-                    .bind("userId", userId)
-                    .mapTo(Integer.class)
-                    .one() > 0;
+            boolean exists = tweetQuries.isLiked(handle, tweetId, userId);
 
             if (exists) {
-                handle.createUpdate("DELETE FROM \"xcloneSchema\".\"like\" WHERE tweet_id = :tweetId AND user_id = :userId")
-                        .bind("tweetId", tweetId)
-                        .bind("userId", userId)
-                        .execute();
+                tweetQuries.removeLike(handle,tweetId,userId);
             } else {
-                handle.createUpdate("INSERT INTO \"xcloneSchema\".\"like\" (tweet_id, user_id, timestamp) VALUES (:tweetId, :userId, NOW())")
-                        .bind("tweetId", tweetId)
-                        .bind("userId", userId)
-                        .execute();
+                tweetQuries.addLike(handle,tweetId,userId);
             }
 
             ctx.redirect("/app/homepage");
---
+++ src/main/java/org/xclone/queries/TweetQuries.java
@@ -0,0 +1,98 @@
+package org.xclone.queries;
+import java.util.List;
+
+import org.jdbi.v3.core.Handle;
+import org.jdbi.v3.core.Jdbi;
+
+import org.jdbi.v3.core.Jdbi;
+import org.xclone.Tweet;
+
+public class TweetQuries {
+    private String tweet_contant_query=
+            "SELECT t.tweet_id, u.username, t.content, t.timestamp, t.location, t.media, t.in_reply_to_tweet_id, COUNT(l.like_id) as like_count " +
+            "FROM \"xcloneSchema\".\"tweet\" t " +
+            "JOIN \"xcloneSchema\".\"user\" u ON t.user_id = u.user_id " +
+            "LEFT JOIN \"xcloneSchema\".\"like\" l ON t.tweet_id = l.tweet_id " +
+            "GROUP BY t.tweet_id, u.username " +
+            "ORDER BY t.timestamp DESC";
+
+    private String user_id=
+            "SELECT user_id FROM \"xcloneSchema\".\"user\" " +
+            "WHERE email = :email";
+
+    private String new_tweet_query=
+            "INSERT INTO \"xcloneSchema\".\"tweet\" " +
+            "(user_id, content, timestamp, location, media, in_reply_to_tweet_id) " +
+            "VALUES (:userId, :content, NOW(), :location, :media, :replyToTweetId)";
+
+    private String like_count_query=
+            "SELECT COUNT(*) FROM \"xcloneSchema\".\"like\" " +
+            "WHERE tweet_id = :tweetId";
+
+    private String remove_like_query=
+            "DELETE FROM \"xcloneSchema\".\"like\" " +
+            "WHERE tweet_id = :tweetId AND user_id = :userId";
+
+    private String add_like=
+            "INSERT INTO \"xcloneSchema\".\"like\" (tweet_id, user_id, timestamp) " +
+            "VALUES (:tweetId, :userId, NOW())";
+
+    public List<Tweet> tweetlist(Jdbi jdbi){
+        return jdbi.withHandle(handle ->
+                handle.createQuery(tweet_contant_query)
+                        .map((rs, mapCtx) -> new Tweet(
+                                rs.getString("tweet_id"),
+                                rs.getString("username"),
+                                rs.getString("content"),
+                                rs.getTimestamp("timestamp"),
+                                rs.getString("location"),
+                                rs.getString("media"),
+                                rs.getString("in_reply_to_tweet_id"),
+                                rs.getInt("like_count")
+                        ))
+                        .list()
+        );
+    }
+
+    public Integer getUserID(Handle handle,String email){
+        return handle.createQuery(user_id)
+                .bind("email", email)
+                .mapTo(Integer.class)
+                .findOne()
+                .orElse(null);
+    }
+
+    public void getNewTweet(Handle handle,Integer userId,String content,String location,String media,int replyToTweetId){
+        handle.createUpdate(new_tweet_query)
+                .bind("userId", userId)
+                .bind("content", content)
+                .bind("location", location)
+                .bind("media", media)
+                .bind("replyToTweetId", replyToTweetId==0 ? null : replyToTweetId)
+                .execute();
+    }
+
+    public boolean isLiked(Handle handle,Integer tweetId,Integer userId){
+        return handle.createQuery(like_count_query)
+                .bind("tweetId", tweetId)
+                .bind("userId", userId)
+                .mapTo(Integer.class)
+                .one() > 0;
+    }
+
+    public void removeLike(Handle handle,int tweetId, int userId){
+        handle.createUpdate(remove_like_query)
+                .bind("tweetId", tweetId)
+                .bind("userId", userId)
+                .execute();
+    }
+
+    public void addLike(Handle handle,int tweetId,int userId){
+        handle.createUpdate(add_like)
+                .bind("tweetId", tweetId)
+                .bind("userId", userId)
+                .execute();
+    }
+
+
+}
